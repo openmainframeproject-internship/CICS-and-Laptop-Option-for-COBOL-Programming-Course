096396aef948978bc11caca75439045aca8dbd0b
Josh Fogus <josh.fogus@gmail.com>
2021-08-10 13:18:14 -0700

    Updated course initialization to update paths inside tasks.json dependent on system.

--- src/cobol-programming-course/OFFHost/Scripts/Init_Course.py
+++ src/cobol-programming-course/OFFHost/Scripts/Init_Course.py
@@ -9,7 +9,9 @@ import os
 import sys
 from shutil import copy2
 from xml.etree import ElementTree
-from utilities.misc import parse_args
+from utilities.input import read_json
+from utilities.output import write_json
+from utilities.misc import parse_args, get_elem_with_prop
 
 
 def init_course(working_dir):
@@ -43,10 +45,30 @@ def init_course(working_dir):
     build_xml_tree.write(build_file)
 
     ##################
-    # 
+    # Update env variables in tasks.json
     ##################
+    if sys.platform.startswith('win32'):
+        ant_home = 'C:\\Program Files (x86)\\Ant\\apache-ant-1.10.11'
+        java_home = 'C:\\Program Files (x86)\\Micro Focus\\Enterprise Developer\\AdoptOpenJDK\\jre'
+        mfant = 'C:\\Program Files (x86)\\Micro Focus\\Enterprise Developer\\bin\\mfant.jar'
+    else:
+        ant_home = '/opt/apache-ant-1.10.11'
+        java_home = '/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.302.b08-0.el8_4.x86_64/jre'
+        mfant = '/opt/microfocus/EnterpriseDeveloper/lib/mfant.jar'
 
+    tasks_file = os.path.join(working_dir, '.vscode', 'tasks.json')
+    tasks_json = read_json(tasks_file)
 
+    build_json = get_elem_with_prop(tasks_json['tasks'], 'label', 'Build Program')
+    build_env = build_json['options']['env']
+    build_env['ANT_HOME'] = ant_home
+    build_env['JAVA_HOME'] = java_home
+
+    ant_args = build_json['args']
+    lib_index = ant_args.index('-lib') + 1
+    ant_args[lib_index] = mfant
+
+    write_json(tasks_file, tasks_json)
 
 
 if __name__ == "__main__":
